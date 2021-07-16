3e3cd4e1407636abcfd1202c4d729cfa9a3a4b30
Lucas <lucasbpereira97@gmail.com>
2021-07-16 16:25:13 -0300

    LGPD Map - Homepage

---
+++ src/pages/_app.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { AppProps } from 'next/app'
+
+import '../styles/globals.scss';
+import '../styles/app.scss';
+
+const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
+  return (
+      <Component {...pageProps} />
+  )
+}
+
+export default MyApp
