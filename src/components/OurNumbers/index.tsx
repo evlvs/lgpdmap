d5bafdbf530e954bdd97013e4169246f22b78eef
Lucas <lucasbpereira97@gmail.com>
2021-07-21 11:42:49 -0300

    created ourNumbers section

--- src/components/OurNumbers/index.tsx
+++ src/components/OurNumbers/index.tsx
@@ -2,9 +2,29 @@ function OurNumbers() {
   return (
     <>
       <section className="ourNumbers">
+        <div className="ballsColored"></div>
+        <div className="ballsOutlined"></div>
         <h2>
           Conheça mais da Lgpd<span>map</span>
-        </h2>    
+        </h2>
+
+        <ul>
+          <li>
+            <h6>mais de</h6>
+            <h5>30<span>mil</span></h5>
+            <p>Clientes ativos</p>
+          </li>
+          <li>
+            <h6>mais de</h6>
+            <h5>200<span>mil</span></h5>
+            <p>Empresas adequadas à LGPD</p>
+          </li>
+          <li>
+            <h6>mais de</h6>
+            <h5>650<span>mil</span></h5>
+            <p>Volumes de dados mapeados</p>
+          </li>
+        </ul>
       </section>
     </>
   )
