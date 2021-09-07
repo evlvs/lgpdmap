792c326b182da0fd5b949cdf9c3c648de2521d68
Lucas Barbosa <lucasbpereira@outlook.com>
2021-09-07 16:12:51 -0300

    prettier fix

--- src/components/StepsPage/index.tsx
+++ src/components/StepsPage/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import styles from './styles.module.scss'
 // import { useSpring, animated } from 'react-spring'
 // import VisibilitySensor from 'react-visibility-sensor'
